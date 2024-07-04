e5a92b1a3f0c75dac7bcbfdd032af36748e3ba74
Shashikant Pipare B <spipare11@gmail.com>
2024-07-05 00:29:14 +0530

    User API minor changes.

--- user/controllers.go
+++ user/controllers.go
@@ -1,144 +1,82 @@
 package user
 
 import (
-	"context"
-
-	"github.com/Jeffail/gabs/v2"
 	"github.com/gofiber/fiber/v2"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
-	"main.go/connection"
+	"main.go/data"
 )
 
 // Signup
 func signUp(c *fiber.Ctx) (err error) {
-	apiBody, err := gabs.ParseJSON([]byte(c.Body()))
+	a := data.New(c)
+	type RequestParams struct {
+		User
+		Password string `json:"password"`
+	}
+	params := RequestParams{}
+	err = c.BodyParser(&params)
 	if err != nil {
-		return c.Status(200).JSON(&fiber.Map{
-			"success": false,
-			"message": "Unable to parse the input parameters.",
-			"error":   err,
-		})
+		return a.Error(err)
+	}
+	if params.Email == "" || params.Password == "" {
+		return a.Error(ErrMissingRequiredParams)
 	}
 	user := User{}
-	if apiBody.Path("email_id").Data() == nil {
-		return c.Status(200).JSON(&fiber.Map{
-			"success": false,
-			"message": "mandatory field not sent - email_id.",
-		})
-	}
-	user.Email = apiBody.Path("email_id").Data().(string)
-	filter := bson.M{"email_id": user.Email}
-	err = connection.MI.DB.Collection("users").FindOne(context.Background(), filter).Decode(&user)
-	if err != nil && err != mongo.ErrNoDocuments {
-		return c.Status(200).JSON(&fiber.Map{
-			"success": false,
-			"message": "unable to check if user exists.",
-			"error":   err,
-		})
+	err = user.getByEmail(params.Email)
+	if err != nil {
+		return a.Error(err)
 	}
 	if user.ID != primitive.NilObjectID {
-		return c.Status(200).JSON(&fiber.Map{
-			"success": false,
-			"message": "user already exists.",
-		})
+		return a.Error(ErrUserAlreadyExists)
 	}
-	if apiBody.Path("first_name").Data() != nil {
-		user.FirstName = apiBody.Path("first_name").Data().(string)
-	}
-	if apiBody.Path("last_name").Data() != nil {
-		user.LastName = apiBody.Path("last_name").Data().(string)
-	}
-	if apiBody.Path("country").Data() != nil {
-		user.Country = apiBody.Path("country").Data().(string)
-	}
-	if apiBody.Path("password").Data() != nil {
-		password := apiBody.Path("password").Data().(string)
-		err := user.encryptPassword(password)
-		if err != nil {
-			return c.Status(200).JSON(&fiber.Map{
-				"status":  false,
-				"message": "Error in generating random string",
-				"error":   err,
-			})
-		}
-	} else {
-		return c.Status(200).JSON(&fiber.Map{
-			"success": false,
-			"message": "Password not sent",
-		})
-	}
-	user.ID = primitive.NewObjectID()
-	res, err := connection.MI.DB.Collection("users").InsertOne(context.Background(), user)
+	user.Email = params.Email
+	user.FirstName = params.FirstName
+	user.LastName = params.LastName
+	user.Country = params.Country
+	err = user.encryptPassword(params.Password)
 	if err != nil {
-		return c.Status(200).JSON(&fiber.Map{
-			"success": true,
-			"message": "unable to sign up the user.",
-			"error":   err,
-		})
+		return a.Error(err)
 	}
+
+	err = user.insertOne()
+	if err != nil {
+		return a.Error(err)
+	}
+
 	auth := Auth{}
 	user.Authentication = auth
-	return c.Status(200).JSON(&fiber.Map{
-		"success":      true,
-		"message":      "user signed up successfully.",
-		"user_details": res,
-	})
+	return a.Data(user)
 }
 
 // login without jwt
 func login(c *fiber.Ctx) (err error) {
-	apiBody, err := gabs.ParseJSON([]byte(c.Body()))
+	a := data.New(c)
+	type RequestParams struct {
+		User
+		Password string `json:"password"`
+	}
+	params := RequestParams{}
+	err = c.BodyParser(&params)
 	if err != nil {
-		return c.Status(200).JSON(&fiber.Map{
-			"success": false,
-			"message": "Unable to parse the input parameters.",
-			"error":   err,
-		})
+		return a.Error(err)
 	}
-	if apiBody.Path("email_id").Data() == nil {
-		return c.Status(200).JSON(&fiber.Map{
-			"success": false,
-			"message": "email_id not sent.",
-		})
-	}
-	if apiBody.Path("password").Data() == nil {
-		return c.Status(200).JSON(&fiber.Map{
-			"success": false,
-			"message": "password not sent.",
-		})
+	if params.Email == "" || params.Password == "" {
+		return a.Error(ErrMissingRequiredParams)
 	}
 	user := User{}
-	user.Email = apiBody.Path("email_id").Data().(string)
-	password := apiBody.Path("password").Data().(string)
-	err, ok := user.authenticateUser(password)
+	user.Email = params.Email
+	err, ok := user.authenticateUser(params.Password)
 	if err != nil {
-		return c.Status(200).JSON(&fiber.Map{
-			"success": false,
-			"message": "unable to authenicate user.",
-			"error":   err,
-		})
+		return a.Error(err)
 	}
 	if !ok {
-		return c.Status(200).JSON(&fiber.Map{
-			"success": false,
-			"message": "Incorrect password.",
-		})
+		return a.Error(ErrIncorrectPassword)
 	}
 	jwtToken, err := user.createToken()
 	if err != nil {
-		return c.Status(200).JSON(&fiber.Map{
-			"success": false,
-			"message": "Error in authenticating user.",
-			"error":   err.Error(),
-		})
+		return a.Error(ErrAuthUser)
 	}
-	return c.Status(200).JSON(&fiber.Map{
-		"success":   true,
-		"message":   "user logged in succesfully.",
-		"jwt_token": jwtToken,
-	})
+	return a.Data(jwtToken)
 }
 
 func logout(c *fiber.Ctx) (err error) {
--- user/methods.go
+++ user/methods.go
@@ -9,36 +9,62 @@ import (
 
 	"github.com/golang-jwt/jwt"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 	"golang.org/x/crypto/pbkdf2"
 	"main.go/connection"
 )
 
-func (u *User) encryptPassword(password string) error {
-	salt, err := generateRandomString(32)
+func (u *User) getByEmail(email string) (err error) {
+	filter := bson.D{
+		{
+			Key:   "email_id",
+			Value: email,
+		},
+	}
+	err = connection.MI.DB.Collection("users").FindOne(context.TODO(), filter).Decode(&u)
 	if err != nil {
-		return err
+		return
+	}
+	return
+}
+func (u *User) insertOne() (err error) {
+	var insertedResult *mongo.InsertOneResult
+	insertedResult, err = connection.MI.DB.Collection("users").InsertOne(context.TODO(), u)
+	if err != nil {
+		return
+	}
+	u.ID, _ = insertedResult.InsertedID.(primitive.ObjectID)
+	return
+}
+
+func (u *User) encryptPassword(password string) (err error) {
+	salt := ""
+	salt, err = generateRandomString(32)
+	if err != nil {
+		return
 	}
 
 	hash := pbkdf2.Key([]byte(password), []byte(salt), 872791, 64, sha512.New)
 	byteKey := []byte(fmt.Sprintf("%s", hash))
 	u.Authentication.Hash = hex.EncodeToString(byteKey)
 	u.Authentication.Token = salt
-	return nil
+	return
 }
 
-func (u *User) authenticateUser(password string) (error, bool) {
-	filter := bson.M{"email_id": u.Email}
-	err := connection.MI.DB.Collection("users").FindOne(context.Background(), filter).Decode(&u)
+func (u *User) authenticateUser(password string) (err error, flag bool) {
+	err = u.getByEmail(u.Email)
 	if err != nil {
-		return err, false
+		return
 	}
 	rawHash := pbkdf2.Key([]byte(password), []byte(u.Authentication.Token), 872791, 64, sha512.New)
 	byteKey := []byte(fmt.Sprintf("%s", rawHash))
 	hash := hex.EncodeToString(byteKey)
-	if hash == u.Authentication.Hash {
-		return nil, true
+	if hash != u.Authentication.Hash {
+		return
 	}
-	return nil, false
+	flag = true
+	return
 }
 
 func (u *User) createToken() (string, error) {
